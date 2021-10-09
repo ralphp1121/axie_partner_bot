675a7204442027218df1314583a985af8c7faa32
Ralphy P <44515819+ralphp1121@users.noreply.github.com>
2021-10-09 12:52:29 +0000

    Added SLP conversion

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ client = discord.Client()
 my_secret = os.environ['TOKEN']
 
 # List of all scholars and ronin addresses
-ronin = {'karl':'ronin:0e2b59addf4e324b3a0c26b62280aa270d00f729','reynald':'ronin:84eca762ecdd0c388c3c9a70baf4b9317241f390', 'mylene':'ronin:3bd8d200ef6d1b82f7c2ddfe6d89fedc5283354c','janjan':'ronin:a5fd8015eb88bac5fbffc074449585307de8f724', 'janrae':'ronin:752e5f460768fb0f6cadd24f06b23ce34976dc37', 'keo':'ronin:55ba81d5f2e6cdaafc56387e5b8c6f4763e7ee78', 'tracy':'ronin:3e69081f866520b56e2684e14e76243406960e78', 'jinky':'ronin:16e2a8e8ea64f3bafae970549f205925afb7f000'}
+ronin = {'karl':'ronin:0e2b59addf4e324b3a0c26b62280aa270d00f729','reynald':'ronin:84eca762ecdd0c388c3c9a70baf4b9317241f390', 'janjan':'ronin:a5fd8015eb88bac5fbffc074449585307de8f724', 'janrae':'ronin:752e5f460768fb0f6cadd24f06b23ce34976dc37', 'keo':'ronin:55ba81d5f2e6cdaafc56387e5b8c6f4763e7ee78', 'tracy':'ronin:3e69081f866520b56e2684e14e76243406960e78', 'jinky':'ronin:16e2a8e8ea64f3bafae970549f205925afb7f000'}
 
 # List of scholar names to be used for the bot commands
-scholars = ["mylene", "jinky", "janjan", "janrae", "keo", "karl", "tracy", "reynald"]
+scholars = ["jinky", "janjan", "janrae", "keo", "karl", "tracy", "reynald"]
 
 # Get the SLP based on the ronin address
 def get_current_slp(ronin_id):
@@ -22,6 +22,14 @@ def get_current_slp(ronin_id):
   json_data = json.loads(response.text)
   curr_slp = json_data['earnings']['slp_inventory']
   return curr_slp
+
+# Get SLP conversion to PHP via Coingecko API
+# Reference: https://www.coingecko.com/en/api/documentation
+def get_current_conversion():
+  response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies=php")
+  json_data = json.loads(response.text)
+  curr_convert = json_data['smooth-love-potion']['php']
+  return curr_convert
 
 # Get next withdrawal date
 def get_next_withdraw(ronin_id):
@@ -51,17 +59,17 @@ def get_allocation(ronin_id):
 
   curr_slp = get_current_slp(ronin_id)
 
-  slp_average = int(curr_slp/diff_date_int)
+  slp_average = int(curr_slp/(diff_date_int+1))
 
 # Return output for daily average SLP and current tier
   if slp_average >= 126:
-    alloc = "Daily SLP Average: "+str(slp_average)+" at 55% earnings, yon oh, lufet!"+":flame:"
+    alloc = "Daily SLP Average: "+str(slp_average)+" at 55% earnings, yon oh, lufet!"+" :flame:"
     return alloc
   elif slp_average < 126 and slp_average > 75:
-    alloc = "Daily SLP Average: "+str(slp_average)+" at 50% earnings, good job, go go go lang!"+":sunglasses:"
+    alloc = "Daily SLP Average: "+str(slp_average)+" at 50% earnings, good job, go go go lang!"+" :sunglasses:"
     return alloc
   else:
-    alloc = "Daily SLP Average: "+str(slp_average)+" at 40% earnings, konting push pa!"+":muscle:"
+    alloc = "Daily SLP Average: "+str(slp_average)+" at 40% earnings, konting push pa!"+" :muscle:"
     return alloc
 
 @client.event 
@@ -88,10 +96,12 @@ async def on_message(message):
     get_slp = get_current_slp(get_ronin_id)
     get_withdraw = get_next_withdraw(get_ronin_id)
     get_alloc = get_allocation(get_ronin_id)
+    get_rate = get_current_conversion()
+    get_converted = get_slp*get_rate
 
-    await message.channel.send("Current SLP : " + str(get_slp) + " " + ":moneybag:")
+    await message.channel.send("Current SLP : " + str(get_slp) + " " + ":moneybag: "+" convert to Php = "+str(get_converted)+" @"+str(get_rate)+" :money_with_wings:")
     await message.channel.send("Next Withdraw Schedule : " + get_withdraw + " " + ":atm:")
-    #await message.channel.send(get_alloc)
+    await message.channel.send(get_alloc)
 
   if msg.startswith('$partners'):
     await message.channel.send(scholars)  
